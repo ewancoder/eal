fbd38907b4c4b1fb2247aa2d031a52a4b7400ce8
ewancoder <ewancoder@gmail.com>
2014-04-17 15:47:47 +0300

    Deleted xarchiver, now using mc :)

--- peal-user.sh
+++ peal-user.sh
@@ -78,7 +78,7 @@ yaourt -S --noconfirm lib32-nvidia-libgl mesa nvidia nvidia-libgl phonon-gstream
 mess "Install Coding software (3/7)"
 yaourt -S --noconfirm python python-matplotlib python-numpy python-scipy python-sphinx tig
 mess "Install Core software (4/7)"
-yaourt -S --noconfirm devilspie dmenu dunst faience-icon-theme feh ffmpegthumbnailer fuse gnome-themes-standard encfs ntfs-3g gxkb jre kalu lm_sensors p7zip pam_mount preload rsync rxvt-unicode screen terminus-font tilda transset-df ttf-dejavu tumbler xorg-server xorg-server-utils xorg-xinit wmii-hg unrar urxvt-perls xarchiver xboomx xclip xcompmgr zsh
+yaourt -S --noconfirm devilspie dmenu dunst faience-icon-theme feh ffmpegthumbnailer fuse gnome-themes-standard encfs ntfs-3g gxkb jre kalu lm_sensors p7zip pam_mount preload rsync rxvt-unicode screen terminus-font tilda transset-df ttf-dejavu tumbler xorg-server xorg-server-utils xorg-xinit wmii-hg unrar urxvt-perls xboomx xclip xcompmgr zsh
 mess "Install Graphics software (5/7)"
 yaourt -S --noconfirm geeqie gource scrot vlc
 mess "Install Internet software (6/7)"
