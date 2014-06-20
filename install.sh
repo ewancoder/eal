982ef3c32107ddd8034b0a29eda3ccd55b5029ae
ewancoder <ewancoder@gmail.com>
2014-06-20 22:42:20 +0300

    VERY important FIX

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ source ceal.sh
 prepare() {
     rm -f $2
     while read -r p; do
-        if ! [[ "$p" == "" ]] && ! [[ ${p:0:1} == "#" ]] && ! [[ ${p:0:3} == "if " ]] && ! [[ ${p:0:2} == "fi" ]] && ! [[ ${p:0:4} == "for " ]] && ! [[ ${p:0:4} == "else" ]] && ! [[ ${p:0:4} == "done" ]]; then
+        if ! [[ "$p" == "" ]] && ! [[ ${p:0:1} == "#" ]] && ! [[ ${p:0:3} == "if " ]] && ! [[ ${p:0:2} == "fi" ]] && ! [[ ${p:0:4} == "for " ]] && ! [[ ${p:0:4} == "else" ]] && ! [[ ${p:0:4} == "done" ]] && ! [[ ${p:0:6} == "echo '" ]] && ! [[ ${p:0:5} == "' >> " ]] && ! [[ ${p:0:4} == "' > " ]]; then
             echo  "until $p; do" >> $2
             echo -e "\tmess -q \"Eror occured. Retry? (y/n)\"\n\tread ans" >> $2
             echo '    if [ "$ans" == "n" ] || [ "$ans" == "N" ]; then' >> $2
