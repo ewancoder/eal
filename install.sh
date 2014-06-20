a57dead4af8b1a4f1c09429b336ce4b17873b4bd
ewancoder <ewancoder@gmail.com>
2014-06-21 00:53:47 +0300

    Fixed titles of errors :)

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ prepare() {
     while read -r p; do
         if ! [[ "$p" == "" ]] && ! [[ ${p:0:1} == "#" ]] && ! [[ ${p:0:3} == "if " ]] && ! [[ ${p:0:2} == "fi" ]] && ! [[ ${p:0:4} == "for " ]] && ! [[ ${p:0:4} == "else" ]] && ! [[ ${p:0:4} == "done" ]] && ! [[ ${p:0:6} == "echo '" ]] && ! [[ ${p:0:5} == "' >> " ]] && ! [[ ${p:0:4} == "' > " ]] && ! [[ ${p:0:5} == "mess " ]]; then
             echo  "until $p; do" >> $2
-            echo -e "\tmess -q \"Eror occured on step [$m]. Retry? (y/n)\"\n\tans=''" >> $2
+            echo -e "\tans=''" >> $2
+            echo '    mess -q "Eror occured on step [$m]. Retry? (y/n)"' >> $2
             if [ $timeout -eq 0 ]; then
                 echo -e "\tread ans" >> $2
             else
