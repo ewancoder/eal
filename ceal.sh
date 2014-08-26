efd0fe5fd8cf333ce3cfc35039357ca7db57ebfa
ewancoder <ewancoder@gmail.com>
2014-08-26 17:54:43 +0300

    Added screensaver (for locking) + xdotool

--- ceal.sh
+++ ceal.sh
@@ -240,9 +240,11 @@ mirror=( Belarus Denmark Russia United France )
         #tig - git pretty viewer
         #tilda - fullscreen terminal on Mod+F12
         #transset-df - transparency-setting tool (see devilspie)
+        #xdotool - tool for moving mouse (grab handler)
         #xorg-server - xserver
         #xorg-server-utils - the most important stuff based on current configuration
         #xorg-xinit - startx tool
+        #xscreensaver - screensaver for locking the session
         #wmii-hg - supercool WM (although I am planning on switching to dwm)
         #unclutter - for hiding the mouse
         #unrar, unzip - default archivers
@@ -305,7 +307,7 @@ mirror=( Belarus Denmark Russia United France )
     software=(
         "lib32-nvidia-libgl mesa nvidia nvidia-libgl phonon-qt4-gstreamer"
         "alsa-plugins alsa-utils lib32-alsa-plugins lib32-libpulse pulseaudio pulseaudio-alsa"
-        "compton cronie cv devilspie udevil dmenu dunst feh fuse git gksu gxkb jmtpfs libnotify mpg123 openssh p7zip rsync rxvt-unicode screen tig tilda transset-df xorg-server xorg-server-utils xorg-xinit wmii-hg unrar unclutter unzip urxvt-perls wpa_supplicant xclip zsh"
+        "compton cronie cv devilspie udevil dmenu dunst feh fuse git gksu gxkb jmtpfs libnotify mpg123 openssh p7zip rsync rxvt-unicode screen tig tilda transset-df wmii-hg unrar unclutter unzip urxvt-perls wpa_supplicant xclip xdotool xorg-server xorg-server-utils xorg-xinit xscreensaver zsh"
         "faience-icon-theme ffmpegthumbnailer gtk-theme-flatstudio terminus-font ttf-dejavu tumbler"
         "canto-curses chromium chromium-pepper-flash copy-agent deluge dropbox-experimental jre8-openjdk icedtea-web net-tools skype"
         "anki calligra-krita geeqie gource gvim kdegraphics-okular libreoffice-calc libreoffice-common libreoffice-impress libreoffice-math libreoffice-writer libreoffice-en-US hyphen hyphen-en hyphen-ru hunspell hunspell-en hunspell-ru mc scrot vlc"
