a9d1a9ca885b83bb699c66af6f178f42df1e5a25
ewancoder <ewancoder@gmail.com>
2014-10-22 19:04:11 +0300

    Added dmenu->dmenu2

--- ceal.sh
+++ ceal.sh
@@ -225,7 +225,7 @@ mirror=( Belarus Denmark Russia United France )
         #cv - util for monitoring current IO usage (used by mine wmii status bar)
         #devilspie - sets transparency to windows based on ruleset (see transset-df)
         #udevil - for automounting removable devices
-        #dmenu - lightweight menu
+        #dmenu2 - lightweight menu fork with WIDTH patch
         #dunst-git - lightweight notification daemon (need git version for fixed URL's commit)
         #feh - image viewer / wallpaper
         #fuse - FUSE itself
@@ -319,7 +319,7 @@ mirror=( Belarus Denmark Russia United France )
     software=(
         "lib32-nvidia-libgl mesa nvidia nvidia-libgl phonon-qt4-gstreamer"
         "alsa-plugins alsa-utils lib32-alsa-plugins lib32-libpulse pulseaudio pulseaudio-alsa"
-        "compton cronie cv devilspie udevil dmenu dunst-git feh fuse git gksu gxkb jmtpfs libnotify mpg123 openssh p7zip rsync rxvt-unicode screen sshfs tig tilda transset-df wmii-hg unrar unclutter unzip urxvt-perls wpa_supplicant xclip xflux xdotool xorg-server xorg-server-utils xorg-xinit xscreensaver-arch-logo zsh"
+        "compton cronie cv devilspie udevil dmenu2 dunst-git feh fuse git gksu gxkb jmtpfs libnotify mpg123 openssh p7zip rsync rxvt-unicode screen sshfs tig tilda transset-df wmii-hg unrar unclutter unzip urxvt-perls wpa_supplicant xclip xflux xdotool xorg-server xorg-server-utils xorg-xinit xscreensaver-arch-logo zsh"
         "faience-icon-theme ffmpegthumbnailer gtk-theme-flatstudio terminus-font ttf-dejavu tumbler"
         "canto-curses-git chromium chromium-pepper-flash copy-agent dropbox-experimental jre8-openjdk icedtea-web net-tools skype"
         "anki calligra-krita geeqie gource gvim kdegraphics-okular libreoffice-calc libreoffice-common libreoffice-impress libreoffice-math libreoffice-writer libreoffice-en-US hyphen hyphen-en hyphen-ru hunspell hunspell-en hunspell-ru mc scrot thunar vlc"
