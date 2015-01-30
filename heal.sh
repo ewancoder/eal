dfbafa6b17e4048b6f5c263b8661dfcccf22e1ee
ewancoder <ewancoder@gmail.com>
2015-01-30 08:00:33 +0300

    Refreshed heal.sh

--- heal.sh
+++ heal.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 source ceal.sh
-mess -t "Prepare live-cd"
-if ! which unsquashfs > /dev/null || ! which curl > /dev/null; then
+mess -t "Installing needed software"
+if which unsquashfs > /dev/null && which curl > /dev/null; then
+    mess "Both squashfs-tools and curl are installed, skipping..."
+else
     mess "Install squashfs-tools and curl"
     if which pacman > /dev/null; then
         pacman -Syy
@@ -9,9 +11,12 @@ if ! which unsquashfs > /dev/null || ! which curl > /dev/null; then
     elif which apt-get > /dev/null; then
         apt-get update
         apt-get -y install squashfs-tools curl
+    else
+        mess -w "Package manager is neither 'pacman' nor 'apt-get'. Can't install tools. Please, make sure that 'squashfs-tools' and 'curl' packages are installed."
     fi
 fi
 
+mess -t "Prepare Arch linux ROOTFS"
 mkdir -p /sfs
 if [ ! "$temp" == "" ]; then
     mess "Mount $temp temporary partition for unsquashing live-cd (must have 1G+ free space)"
@@ -41,6 +46,6 @@ cp -L /etc/resolv.conf /arch/etc
 
 mess -t "Chroot into live-cd environment and execute eal.sh (start regular installation)"
 mess "Copy {eal,ceal,peal}.sh scripts to /arch/root/"
-cp {eal,ceal,peal}.sh /arch/root/
+cp {eal,ceal,peal}.sh makepkg.patch /arch/root/
 mess "Chroot into /arch and execute /root/eal.sh"
 chroot /arch /root/eal.sh
