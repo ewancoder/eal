f0824925058702f616064f3c1bdd2acae373da1d
ewancoder <ewancoder@gmail.com>
2015-08-01 16:03:20 +0300

    Reformatted code in install.sh

--- install.sh
+++ install.sh
@@ -13,46 +13,59 @@ prepare() {
     inside=0
     rm -f $2
     while read -r p; do
-        if ! [ "$p" == "" -o \
-        "${p:0:1}" == "#" -o \
-        "${p:0:2}" == "fi" -o \
-        "${p:0:3}" == "if " -o \
-        "${p:0:4}" == "for " -o \
-        "${p:0:4}" == "else" -o \
-        "${p:0:4}" == "done" -o \
-        "${p:0:4}" == "' > " -o \
-        "${p:0:5}" == "mess " -o \
-        "${p:0:7}" == "source " -o \
-        "${p:0:11}" == "cd \`dirname" -o \
-        "${p:0:5}" == "elif " ]; then
+        if [ "$p" == ""              \
+          -o "${p:0:1}" == "#"       \
+          -o "${p:0:2}" == "fi"      \
+          -o "${p:0:3}" == "if "     \
+          -o "${p:0:4}" == "for "    \
+          -o "${p:0:4}" == "else"    \
+          -o "${p:0:4}" == "done"    \
+          -o "${p:0:4}" == "' > "    \
+          -o "${p:0:5}" == "mess "   \
+          -o "${p:0:5}" == "elif "   \
+          -o "${p:0:7}" == "source " \
+          -o "${p:0:11}" == "cd \`dirname" ]; then  
+            echo $p                                                             >> $2
+        else
             if [ $verbose -eq 1 ]; then
                 str=`echo $p | sed "s/'/'\"'\"'/g"`
                 if [ "${p:0:7}" == "echo $'" ]; then
                     inside=1
-                    echo $p >> $2
+                    echo $p                                                     >> $2
                     continue
                 elif [ "${p:0:5}" == "' >> " ]; then
                     inside=0
-                    echo $p >> $2
+                    echo $p                                                     >> $2
                     continue
                 else
                     if [ $inside -eq 1 ]; then
-                        echo "mess -v \\'$str\\'" >> $2
+                        echo "mess -v \\'$str\\'"                               >> $2
                     else
-                        echo "mess -v '$str'" >> $2
+                        echo "mess -v '$str'"                                   >> $2
                     fi
                 fi
             fi
-            echo "until $p; do" >> $2
-            echo -e '    ans=""\n    mess -q "Error occured on step [$step]. Retry? (y/n)"' >> $2
+            echo "until $p; do"                                                 >> $2
+            echo -e '    ans=""'                                                >> $2
+            echo -e '    mess -q "Error occured on step [$step]. Retry? (y/n)"' >> $2
             if [ $timeout -eq 0 ]; then
-                echo -e "    read ans" >> $2
+                echo -e "    read ans"                                          >> $2
             else
-                echo -e "    mess -q \"Auto-repeating in $timeout seconds\"\n    read -t $timeout ans" >> $2
+                echo -e "    mess -q \"Auto-repeating in $timeout seconds\""    >> $2
+                echo -e "    read -t $timeout ans"                              >> $2
             fi
-            echo -e '    if [ "$ans" == "n" -o "$ans" == "N" ]; then\n        break\n    elif [ "$ans" == "givemebash" ]; then\n        /bin/bash\n        ans=""\n        mess -q "Retry [$step]? (y/n)"\n        read ans\n        if [ "$ans" == "n" -o "$ans" == "N" ]; then\n            break\n        fi\n    fi\ndone' >> $2
-        else
-            echo $p >> $2
+            echo -e '    if [ "$ans" == "n" -o "$ans" == "N" ]; then'           >> $2
+            echo -e '        break'                                             >> $2
+            echo -e '    elif [ "$ans" == "givemebash" ]; then'                 >> $2
+            echo -e '        /bin/bash'                                         >> $2
+            echo -e '        ans=""'                                            >> $2
+            echo -e '        mess -q "Retry [$step]? (y/n)"'                    >> $2
+            echo -e '        read ans'                                          >> $2
+            echo -e '        if [ "$ans" == "n" -o "$ans" == "N" ]; then'       >> $2
+            echo -e '            break'                                         >> $2
+            echo -e '        fi'                                                >> $2
+            echo -e '    fi'                                                    >> $2
+            echo -e 'done'                                                      >> $2
         fi
     done < $1
 }
