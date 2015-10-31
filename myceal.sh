c092ac1f9cb6a59c8fd1c18c3c83d2c011ad679d
ewancoder <ewancoder@gmail.com>
2015-10-31 21:41:10 +0300

    Removed vlc/mplayer in favor of mpv, skype in favor of slack

--- myceal.sh
+++ myceal.sh
@@ -53,12 +53,12 @@
     software=(
         "lib32-nvidia-libgl mesa nvidia nvidia-libgl phonon-qt5-gstreamer ttf-dejavu"
         "alsa-plugins alsa-utils lib32-alsa-plugins lib32-libpulse pulseaudio pulseaudio-alsa"
-        "cronie devilspie udevil feh fuse git gksu keychain libnotify mplayer nawk openssh p7zip pygtk redshift rsync rxvt-unicode screen sshfs tig tilda transset-df unrar unclutter unzip urxvt-perls wpa_supplicant xclip xdotool xorg-server xorg-server-utils xorg-xinit zsh"
+        "cronie devilspie udevil feh fuse git gksu keychain libnotify mpv nawk openssh p7zip pygtk redshift rsync rxvt-unicode screen sshfs tig tilda transset-df unrar unclutter unzip urxvt-perls wpa_supplicant xclip xdotool xorg-server xorg-server-utils xorg-xinit zsh"
         "faience-icon-theme ffmpegthumbnailer terminus-font tumbler"
         "chromium deluge jre8-openjdk icedtea-web net-tools skype"
-        "anki calligra-krita filelight geeqie gource vim impressive kdegraphics-okular libreoffice-fresh hyphen hyphen-en hunspell hunspell-en mc scrot thunar vlc"
+        "anki calligra-krita filelight geeqie gource vim impressive kdegraphics-okular libreoffice-fresh hyphen hyphen-en hunspell hunspell-en mc scrot thunar"
         "ctags mono pygmentize python python-matplotlib python-numpy python-pyserial python-requests python-scipy python-sphinx python2-pygments texlive-core texlive-humanities texlive-langcyrillic texlive-latexextra texlive-pictures texlive-science wine"
         "dosfstools encfs gparted ntfs-3g smartmontools thefuck virtualbox"
     )
     service=( cronie deluged systemd-networkd systemd-timesyncd )
-    build=( compton progress dmenu2 dropbox dunst-git gtk-theme-espresso gcalcli gxkb slimlock-git slim-archlinux-solarized-spiral hyphen-ru hunspell-ru-aot jmtpfs latex-beamer pencil popcorntime-bin syncplay pasystray-git xcape oh-my-zsh-git )
+    build=( compton progress dmenu2 dropbox dunst-git gtk-theme-espresso gcalcli gxkb slimlock-git slim-archlinux-solarized-spiral hyphen-ru hunspell-ru-aot jmtpfs latex-beamer pencil popcorntime-bin slack-desktop syncplay pasystray-git xcape )
