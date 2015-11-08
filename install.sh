b5830ad00b9421075a757237f7ada9a929610c4c
ewancoder <ewancoder@gmail.com>
2015-11-08 23:02:55 +0300

    Cleaned install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ceal.sh
 source myceal.sh
-if [ ! `id -u` -eq 0 ]; then
+if [ ! "$(id -u)" -eq 0 ]; then
     mess -w "You have to be ROOT to run this script. Exiting."
     exit
 fi
@@ -11,70 +11,92 @@ mess -w "Before proceeding:\n\t1) Edit 'myceal.sh' configuration file\n\t2) Form
 source myceal.sh
 
 prepare() {
-    rm -f $2
+    rm -f "$2"
     while read -r p; do
-        if [ "$p" == ""              \
-          -o "${p:0:1}" == "#"       \
-          -o "${p:0:2}" == "fi"      \
-          -o "${p:0:3}" == "if "     \
-          -o "${p:0:4}" == "for "    \
-          -o "${p:0:4}" == "else"    \
-          -o "${p:0:4}" == "done"    \
-          -o "${p:0:4}" == "' > "    \
-          -o "${p:0:5}" == "mess "   \
-          -o "${p:0:5}" == "elif "   \
-          -o "${p:0:7}" == "source " \
-          -o "${p:0:11}" == "cd \`dirname" ]; then  
-            echo $p                                                             >> $2
+        if [ "$p" == "" ] ||
+           [ "${p:0:1}" == "#" ] ||
+           [ "${p:0:2}" == "fi" ] ||
+           [ "${p:0:3}" == "if " ] ||
+           [ "${p:0:4}" == "for " ] ||
+           [ "${p:0:4}" == "else" ] ||
+           [ "${p:0:4}" == "done" ] ||
+           [ "${p:0:4}" == "' > " ] ||
+           [ "${p:0:5}" == "mess " ] ||
+           [ "${p:0:5}" == "elif " ] ||
+           [ "${p:0:7}" == "source " ] ||
+           [ "${p:0:11}" == "cd \`dirname" ]; then  
+            echo "$p" >> "$2"
         else
-
-            cmd=`echo $p | sed -r 's/(.)/\\\\\1/g'`
-            echo "cmd=\$(echo $cmd)"                                            >> $2
+            cmd=$(echo "$p" | sed -r 's/(.)/\\\\\1/g')
+            echo "cmd=\$(echo $cmd)" >> "$2"
 
             if [ $substitute -eq 1 ]; then
                 parsed=$(perl -pe 's/\$(?:{.*?}|\w+)(*SKIP)(*F)|(.)/\\$1/g' <<< "$p")
             else
                 parsed=$cmd
             fi
-            echo "parsed=\$(echo $parsed)"                                      >> $2
+            echo "parsed=\$(echo $parsed)" >> "$2"
 
             if [ $verbose -eq 1 ]; then
-                echo 'mess -v "$cmd"'                                           >> $2
+                # shellcheck disable=SC2016
+                echo 'mess -v "$cmd"' >> "$2"
                 if [ $auto -eq 0 ]; then
-                    echo -e 'read -rep $'"'"'\\e[33m-> '"'"' -i "$parsed" parsed' >> $2
-                    echo -e 'echo $'"'"'\\e[0m'"'"''                          >> $2
+                    {
+                        # shellcheck disable=SC2016
+                        echo -e 'read -rep $'"'"'\\e[33m-> '"'"' -i "$parsed" parsed'
+                        echo -e 'echo $'"'"'\\e[0m'"'"''
+                    } >> "$2"
                 fi
             fi
-            echo -e 'until eval "$parsed"; do'                                  >> $2
-            echo -e '    ans=""'                                                >> $2
-            echo -e '    mess -q "Error occured on step [$step]. Retry? (y/n)"' >> $2
+            {
+                # shellcheck disable=SC2016
+                echo -e 'until eval "$parsed"; do'
+                echo -e '    ans=""'
+                # shellcheck disable=SC2016
+                echo -e '    mess -q "Error occured on step [$step]. Retry? (y/n)"'
+            } >> "$2"
             if [ $timeout -eq 0 ]; then
-                echo -e "    read ans"                                          >> $2
+                echo -e "    read ans" >> "$2"
             else
-                echo -e "    mess -q \"Auto-repeating in $timeout seconds\""    >> $2
-                echo -e "    read -t $timeout ans"                              >> $2
+                {
+                    echo -e "    mess -q \"Auto-repeating in $timeout seconds\""
+                    echo -e "    read -t $timeout ans"
+                } >> "$2"
             fi
-            echo -e '    if [ "$ans" == "n" -o "$ans" == "N" ]; then'           >> $2
-            echo -e '        break'                                             >> $2
-            echo -e '    elif [ "$ans" == "givemebash" ]; then'                 >> $2
-            echo -e '        /bin/bash'                                         >> $2
-            echo -e '        ans=""'                                            >> $2
-            echo -e '        mess -q "Retry [$step]? (y/n)"'                    >> $2
-            echo -e '        read ans'                                          >> $2
-            echo -e '        if [ "$ans" == "n" -o "$ans" == "N" ]; then'       >> $2
-            echo -e '            break'                                         >> $2
-            echo -e '        fi'                                                >> $2
-            echo -e '    elif [ "$ans" == "EXIT" ]; then'                       >> $2
-            echo -e '        exit'                                              >> $2
-            if [ $verbose -eq 1 ]; then
-                echo -e '    elif [ ! "$ans" == "" ] || [ $auto -eq 0 ]; then'  >> $2
-                echo -e '        read -rep $'"'"'\\e[33m-> '"'"' -i "$parsed" parsed' >> $2
-                echo -e '        echo $'"'"'\\e[0m'"'"''                        >> $2
+            {
+                # shellcheck disable=SC2016
+                echo -e '    if [ "$ans" == "n" -o "$ans" == "N" ]; then'
+                echo -e '        break'
+                # shellcheck disable=SC2016
+                echo -e '    elif [ "$ans" == "givemebash" ]; then'
+                echo -e '        /bin/bash'
+                echo -e '        ans=""'
+                # shellcheck disable=SC2016
+                echo -e '        mess -q "Retry [$step]? (y/n)"'
+                echo -e '        read ans'
+                # shellcheck disable=SC2016
+                echo -e '        if [ "$ans" == "n" -o "$ans" == "N" ]; then'
+                echo -e '            break'
+                echo -e '        fi'
+                # shellcheck disable=SC2016
+                echo -e '    elif [ "$ans" == "EXIT" ]; then'
+                echo -e '        exit'
+            } >> "$2"
+            if [ "$verbose" -eq 1 ]; then
+                {
+                    # shellcheck disable=SC2016
+                    echo -e '    elif [ ! "$ans" == "" ] || [ $auto -eq 0 ]; then'
+                    # shellcheck disable=SC2016
+                    echo -e '        read -rep $'"'"'\\e[33m-> '"'"' -i "$parsed" parsed'
+                    echo -e '        echo $'"'"'\\e[0m'"'"''
+                } >> "$2"
             fi
-            echo -e '    fi'                                                    >> $2
-            echo -e 'done'                                                      >> $2
+            {
+                echo -e '    fi'
+                echo -e 'done'
+            } >> "$2"
         fi
-    done < $1
+    done < "$1"
 }
 
 mess -t "Prepare installation scripts (add error handling)"
@@ -98,9 +120,9 @@ if [ ! "$rootscript" == "" ]; then
     source myceal.sh
     mess -t "ROOT executed script after installation"
     ' > root.sh
-    cat root.sh $rootscript > temp && mv temp $rootscript
+    cat root.sh "$rootscript" > temp && mv temp "$rootscript"
     rm root.sh
-    prepare $rootscript eal/$rootscript
+    prepare "$rootscript" "eal/$rootscript"
 fi
 
 mess "Prepare user-executed scripts"
@@ -112,9 +134,9 @@ for (( i = 0; i < ${#user[@]}; i++ )); do
         source myceal.sh
         mess -t 'User script ${user[$i]}'
         " > user.sh
-        cat user.sh ${userscript[$i]} > temp && mv temp ${userscript[$i]}
+        cat user.sh "${userscript[$i]}" > temp && mv temp "${userscript[$i]}"
         rm user.sh
-        prepare ${userscript[$i]} eal/${userscript[$i]}
+        prepare "${userscript[$i]}" "eal/${userscript[$i]}"
     fi
 done
 
@@ -131,7 +153,7 @@ done
 #    prepare after.sh eal/after.sh
 #fi
 
-cd eal
+cd eal || exit
 
 mess -t "Start installation"
 mess "Make all scripts executable"
